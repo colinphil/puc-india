f2f326b4d7c9e5f0fea4b47c5d7c1f1d63f50d8d
cphillips17 <55683870+cphillips17@users.noreply.github.com>
2020-10-13 13:14:09 -0500

    update config

--- gridsome.config.js
+++ gridsome.config.js
@@ -6,7 +6,16 @@
 
 module.exports = {
   siteName: "PUC India",
-  plugins: [],
+  plugins: [
+    {
+      use: '@gridsome/plugin-critical',
+      options:{
+        paths: ['/'],
+        width: 1300,
+        height: 900,
+      }
+    }
+  ],
   siteUrl: "https://puc-india.com",
   siteDescription: 'PUC India is an interdiscplinary team of undergraduate engineering students in Projects with Underserved Communities, a humanitarian engineering organization at the Univeristy of Texas at Austin. The team is designing, funding, and constructing crucial infrastructure for the village of Siripudi in Andhra Pradesh, India.',
   pathPrefix: "/puc-india",
